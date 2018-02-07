b3c5958a875af971c7e89d6228b68e846f55649f
youjing <onizudango@gmail.com>
2018-02-07 14:02:49 +0800

    msg list

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,17 +1,35 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Message } from '../../models/message';
+import { AuthProvider } from '../../providers/auth/auth';
+import { AngularFireDatabase, AngularFireList, AngularFireAction } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
+
 
 @Component({
-  selector: 'page-contact',
-  templateUrl: 'contact.html'
+	selector: 'page-contact',
+	templateUrl: 'contact.html'
 })
 export class ContactPage {
+	messages: Observable<any[]>;
+	msgsRef: AngularFireList<any>;
 
-  constructor(public navCtrl: NavController) {
+	constructor(public navCtrl: NavController, public authProvider: AuthProvider, afDatabase: AngularFireDatabase) {
+		this.messages = afDatabase.list(`/profile/${authProvider.currentUID()}/messages`).valueChanges();
+		this.msgsRef = afDatabase.list(`/profile/${authProvider.currentUID()}/messages`);
+	}
 
-  }
+	addMsg() {
+		/*
+		var message = {uid: this.authProvider.currentUID(), content: "hehahah"} as Message;
+		this.messages.push(message);
+		*/
 
-
-  
+		const newMsgRef = this.msgsRef.push({});
+		newMsgRef.set({
+			uid: this.authProvider.currentUID(),
+			content: "hehahah"
+		})
+	}
 
 }
