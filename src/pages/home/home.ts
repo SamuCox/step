d549f5261ae778537ce15f74ac6a92fec524f65c
youjing <onizudango@gmail.com>
2018-02-22 08:26:28 +0800

    home page step update

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { NavController, Platform } from 'ionic-angular';
 //import { LocalNotifications } from '@ionic-native/local-notifications';
 import { AngularFireAuth } from 'angularfire2/auth';
-import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 import { Profile } from '../../models/profile';
 import { LoginPage } from '../login/login'
 import { Health } from '@ionic-native/health';
+import { Chart } from 'chart.js';
+import { AuthProvider } from '../../providers/auth/auth';
 
 
 
@@ -16,12 +18,127 @@ import { Health } from '@ionic-native/health';
 
 export class HomePage {
 
+	@ViewChild('doughnutCanvas') doughnutCanvas;
+	doughnutCanvas: any;
+	msgsRef: AngularFireList<any>;
+
 	profile = {username: "haa", sex: "female"} as Profile;
-	step = "jljljl";
+	step = "";
 
-	constructor(public navCtrl: NavController, private afAuth: AngularFireAuth, private health: Health, private afDatabase: AngularFireDatabase) {
+	constructor(public navCtrl: NavController, public platform: Platform, public authProvider: AuthProvider, private afAuth: AngularFireAuth, private health: Health, private afDatabase: AngularFireDatabase) {
+		this.platform.ready().then((readySource) => {
+			this.health.requestAuthorization([
+				'steps'
+				])
+			.then(res => {
+				console.log("requested " + res);
+				this.health.queryAggregated({
+  		startDate: new Date(new Date().getTime() - 24 * 60 * 60 * 1000), // three days ago
+  		endDate: new Date(), // now
+  		dataType: 'steps',
+  		bucket: 'day'})
+				.then(res => {
+					this.step = res[1].value;
+					this.generateStepChart(parseInt(this.step));
+					this.uploadStep();
 
-	}	
+				/*
+				for(var property in res[1]) {
+					console.log(property + "=" + res[1][property]);
+					this.step = res[1].value;
+				}*/
+
+			})
+				.catch(e => {console.log("steps err " + e)})
+				.catch(e => console.log("e1"+e));
+			}
+			);
+		});
+	}
+
+	ionViewDidLoad() {
+		this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
+			type: 'doughnut',
+			data: {
+				datasets: [{
+					data: [12],
+					backgroundColor: [
+					'rgba(255, 99, 132, 0.2)'
+					]
+				}]
+			}
+		});
+
+	}
+
+	uploadStep() {
+		var today = new Date();
+		var mm = today.getMonth();
+		var dd = today.getDate();
+		this.msgsRef = this.afDatabase.list(`/profile/${this.authProvider.currentUID()}/steps`);
+		const newMsgRef = this.msgsRef.push({});
+		newMsgRef.set({
+			month: mm,
+			date: dd,
+			step: this.step
+		})
+	}
+/*
+	ionViewDidEnter() {
+		this.health.requestAuthorization([
+			'steps'
+			])
+		.then(res => {
+			console.log("requested " + res);
+			this.health.queryAggregated({
+  		startDate: new Date(new Date().getTime() - 24 * 60 * 60 * 1000), // three days ago
+  		endDate: new Date(), // now
+  		dataType: 'steps',
+  		bucket: 'day'})
+			.then(res => {
+				//this.generateStepChart();
+				for(var property in res[1]) {
+					console.log(property + "=" + res[1][property]);
+					this.step = res[1].value;
+				}})
+			.catch(e => {console.log("steps err " + e)})
+			.catch(e => console.log("e1"+e));
+		}
+		);
+	}*/
+
+	generateStepChart(step: number) {
+		console.log("start generation: " + step);
+		if (step < 10000) {
+			this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
+				type: 'doughnut',
+				data: {
+					datasets: [{
+						data: [step, 10000],
+						backgroundColor: [
+						'rgba(255, 99, 132, 0.2)',
+						'rgba(54, 162, 235, 0.2)'
+						]
+					}]
+				}
+			})
+		} else {
+			var n = step / 10000;
+			this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
+				type: 'doughnut',
+				data: {
+					datasets: [{
+						data: [step, n * 10000],
+						backgroundColor: [
+						'rgba(255, 99, 132, 0.2)',
+						'rgba(54, 162, 235, 0.2)'
+						]
+					}]
+				}
+			})
+		}
+	}
+
 
 	signOut() {
 		this.afAuth.auth.signOut();
@@ -68,7 +185,7 @@ export class HomePage {
 						.then(res => console.log("steps available: " + res))
 						.catch(e => console.log("what's wrong " + e));
 
-						this.health.query({
+						/*this.health.query({
   		startDate: new Date(new Date().getTime() - 10000000), // three days ago
   		endDate: new Date(), // now
   		dataType: 'steps'})
@@ -77,6 +194,19 @@ export class HomePage {
 								console.log(property + "=" + res[0][property]);
 							}
 							console.log("steps: " + res.value);})
+						.catch(e => {console.log("steps err " + e)});})
+						.catch(e => console.log("e1"+e));*/
+
+						this.health.queryAggregated({
+  		startDate: new Date(new Date().getTime() - 24 * 60 * 60 * 1000), // three days ago
+  		endDate: new Date(), // now
+  		dataType: 'steps',
+  		bucket: 'day'})
+						.then(res => {
+							for(var property in res[1]) {
+								console.log(property + "=" + res[1][property]);
+								this.step = res[1].value;
+							}})
 						.catch(e => {console.log("steps err " + e)});})
 					.catch(e => console.log("e1"+e));
 
