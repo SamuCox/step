09e0ab80f887f575aaa5af4448ecd9cd63752758
youjing <onizudango@gmail.com>
2018-03-26 22:02:22 +0800

    card css

--- src/components/message-graph/message-graph.ts
+++ src/components/message-graph/message-graph.ts
@@ -92,10 +92,10 @@ import { ViewController } from 'ionic-angular';
           },
           layout :{
             padding: {
-                left: 0,
-                right: 0,
-                top: 0,
-                bottom: 0
+              left: 0,
+              right: 0,
+              top: 0,
+              bottom: 0
             }
           },
           scales: {
