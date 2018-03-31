1fe7af4a561186f0f4f65b5c7710c07f4e4fff5e
youjing <onizudango@gmail.com>
2018-03-31 23:12:10 +0800

    alternative1: scroll questionnaire

--- src/components/message-survey/message-survey.ts
+++ src/components/message-survey/message-survey.ts
@@ -1,6 +1,6 @@
-import { Component, Input, ViewChild } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef } from '@angular/core';
 import { AngularFireDatabase, AngularFireList, AngularFireAction, DatabaseSnapshot } from 'angularfire2/database';
-import { Slides } from 'ionic-angular';
+import { Slides, Scroll } from 'ionic-angular';
 
 /**
  * Generated class for the MessageSurveyComponent component.
@@ -8,20 +8,21 @@ import { Slides } from 'ionic-angular';
  * See https://angular.io/api/core/Component for more info on Angular
  * Components.
  */
-@Component({
-  selector: 'message-survey',
-  templateUrl: 'message-survey.html'
-})
-export class MessageSurveyComponent {
+ @Component({
+   selector: 'message-survey',
+   templateUrl: 'message-survey.html'
+ })
+ export class MessageSurveyComponent {
 
-  private _survey: any;
-  @ViewChild(Slides) slides: Slides;
-  @ViewChild('barCanvas') barCanvas;
- 	@Input() set survey(value: any) {
- 		this._survey = value;
- 		console.log("msg came in!");
- 		console.log("msg wut" + value.length);
- 		this.initializeAnswerArrays(value.length);
+   private _survey: any;
+   @ViewChild(Slides) slides: Slides;
+   @ViewChild('barCanvas') barCanvas;
+   @ViewChild('surveyScroll') scroll: ElementRef;
+   @Input() set survey(value: any) {
+     this._survey = value;
+     console.log("msg came in!");
+     console.log("msg wut" + value.length);
+     this.initializeAnswerArrays(value.length);
 
  		// initialize answer based on question type
  	}
@@ -31,37 +32,60 @@ export class MessageSurveyComponent {
 
  	public answers: (string|number)[] = [0];
  	public hasAnswered: boolean[] = [false];
-  public likertScales = [1,2,3,4,5,6,7];
+   public likertScales = [1,2,3,4,5,6,7];
+   public currentQID = 0;
 
- 	valid = true;
+   valid = true;
 
-  constructor() {
-  }
-
-  initializeAnswerArrays(length: number) {
-  	var answers = new Array(length).fill(4);
-  	this.hasAnswered = new Array(length).fill(false);
-  	this._survey.forEach(function (question, index) {
- 			if (question.type == "field") {
- 				answers[index] = ""
- 			}
- 		});
- 		this.answers = answers;
-  }
-
-  public isValid() {
-  	return this.hasAnswered.every(answered => answered);
-  }
-
-  public mcqAnswer() {
-  	console.log("emm");
-  }
-
-  nextQuestion() {
-     this.slides.slideNext(500, true);
+   constructor() {
    }
 
-  prevQuestion() {
-    this.slides.slidePrev(500, true);
+   initializeAnswerArrays(length: number) {
+     var answers = new Array(length).fill(4);
+     this.hasAnswered = new Array(length).fill(false);
+     this._survey.forEach(function (question, index) {
+       if (question.type == "field") {
+         answers[index] = ""
+       }
+     });
+     this.answers = answers;
    }
-}
+
+   public isValid() {
+     return this.hasAnswered.every(answered => answered);
+   }
+
+   public mcqAnswer() {
+     console.log("emm");
+   }
+
+   scrollSurvey() {
+     console.log("我特么scroll了啊！");
+     this.currentQID = Math.floor(this.scroll.nativeElement.scrollTop / 210);
+     console.log("current qid: " + this.currentQID);
+     //this.scroll.nativeElement.scrollTop = 200 * this.currentQID;
+   }
+
+   nextQuestion() {
+     if (this.currentQID < this.survey.length - 1) {
+       this.currentQID++;
+       this.scrollToQuestion(this.currentQID);
+     }
+     console.log("current qid: " + this.currentQID);
+   }
+
+   prevQuestion() {
+     if (this.currentQID > 0) {
+       this.currentQID--;
+       this.scrollToQuestion(this.currentQID);
+     }
+     console.log("current qid: " + this.currentQID);
+   }
+
+   scrollToQuestion(index: number) {
+     console.log("scrolling!!!!");
+     try {
+       this.scroll.nativeElement.scrollTop = 210 * index;
+     } catch(err) { console.log("scroll err " + err)} 
+   }
+ }
